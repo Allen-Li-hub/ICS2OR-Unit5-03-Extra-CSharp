8fd971d9d57c78c30432683e2bf0555f961ed186
Allen Li <113055658+Allen-Li-hub@users.noreply.github.com>
2022-12-14 18:49:46 -0500

    change

--- main.cs
+++ main.cs
@@ -9,7 +9,33 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // user input.
+        int userAge;
+
+        Console.WriteLine("What movie can you watch in your age?");
+        Console.WriteLine("");
+        Console.Write("Enter your age to test it: ");
+        Console.WriteLine("");
+        userAge = Convert.ToInt16(Console.ReadLine());
+
+        Console.WriteLine(" ");
+        if (userAge >= 18)
+        {
+            Console.WriteLine("You can see G, PG and R rated movies!");
+        }
+        else if (userAge >= 13, userAge < 18)
+        {
+            Console.WriteLine("You can see G and PG rated movies!");
+        }
+        else if (userAge <= 12)
+        {
+            Console.WriteLine("You can see G rated movies!");
+        }
+        else
+        {
+            Console.WriteLine("Error!");
+        }
+
         Console.WriteLine("\nDone.");
     }
 }
